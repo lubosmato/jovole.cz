0b859e3d4684329bc2e02ff9138a4219ca56cda2
lubosmato <lubos.matejcik@gmail.com>
2025-04-27 08:08:13 +0200

    feat: add icon

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -11,6 +11,11 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
 
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/icon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Jo vole!</title>
+      </head>
       <body>
         <main>{children}</main>
       </body>
