8f95e60c27f235ac3b7bf788793ab997a7c98760
lubosmato <lubos.matejcik@gmail.com>
2025-04-27 08:13:30 +0200

    feat: improve caching

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -3,6 +3,8 @@ import { getPayload } from "payload"
 import config from "@/payload.config"
 import { notFound, redirect, RedirectType } from "next/navigation"
 
+export const revalidate = 60
+
 export default async function HomePage() {
   const payloadConfig = await config
   const payload = await getPayload({ config: payloadConfig })
