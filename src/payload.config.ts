ce12530ae12141eb8229649e3223d77d46e67490
lubosmato <lubos.matejcik@gmail.com>
2025-04-27 07:56:32 +0200

    fix: db env

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -30,7 +30,7 @@ export default buildConfig({
   },
   db: postgresAdapter({
     pool: {
-      connectionString: process.env.DATABASE_URI || "",
+      connectionString: process.env.DATABASE_URL || "",
     },
   }),
   sharp,
