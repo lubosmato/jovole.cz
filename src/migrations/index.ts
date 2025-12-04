c5fa49db31946c761d2a69c55239f3e6d030988c
lubosmato <lubos.matejcik@gmail.com>
2025-12-04 12:50:53 +0100

    chore: bump payload

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250427_055026 from './20250427_055026';
+import * as migration_20251204_115014 from './20251204_115014';
 
 export const migrations = [
   {
     up: migration_20250427_055026.up,
     down: migration_20250427_055026.down,
-    name: '20250427_055026'
+    name: '20250427_055026',
+  },
+  {
+    up: migration_20251204_115014.up,
+    down: migration_20251204_115014.down,
+    name: '20251204_115014'
   },
 ];
